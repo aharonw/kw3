0d28a498f5202e9c81d744932d8b06e76eaabc84
dmotz <motzdc@gmail.com>
2013-02-03 02:14:08 -0500

    sorting and updating for all metrics

--- public/js/keepwinging.js
+++ public/js/keepwinging.js
@@ -1,7 +1,12 @@
 var socket = io.connect('http://localhost'),
     $grid,
     $streamHead,
-    $total;
+    $total,
+    $select,
+    displayMode = 'all',
+    totals = {},
+    $totals = {},
+    metrics = ['all', 'wings', 'beer', 'brownies'];
 
 socket.on('newUser', function(user){
     addFeedItem(user.text);
@@ -9,11 +14,26 @@ socket.on('newUser', function(user){
 
 socket.on('tap', function(tap){
     addFeedItem(tap.text);
-    $grid.find('.rfid' + tap.rfid).find('.number')
-        .text(tap.wings).end().end()
-        .isotope('updateSortData', $grid.find('li'))
-        .isotope({ sortBy : 'wings' });
-    $total.text(tap.total);
+
+    var $user = $grid.find('.rfid' + tap.rfid),
+        foodTotal = parseInt($user.attr('data-' + tap.food), 10),
+        allTotal = parseInt($user.attr('data-all'), 10);
+
+    $user.attr('data-' + tap.food, foodTotal += tap.num);
+    $user.attr('data-all', allTotal += tap.num);
+    if(displayMode === tap.food){
+        $user.find('.number').text(foodTotal);
+    }else if(displayMode === 'all'){
+        $user.find('.number').text(allTotal);
+    }
+    $grid.isotope('updateSortData', $grid.find('li'))
+        .isotope({ sortBy : displayMode });
+
+    var total = totals[tap.food] += tap.num;
+    totals.all += tap.num;
+    $totals[tap.food].text(total);
+    $totals.all.text(totals.all);
+
 });
 
 function addFeedItem(text){
@@ -24,13 +44,40 @@ function addFeedItem(text){
     $feedItem.slideDown();
 }
 
+var sortFunctions = (function(){
+    var functions = {};
+    for(var i = 0, l = metrics.length; i < l; i++){
+        (function(metric){
+            functions[metric] = function($el){
+                return parseInt($el.attr('data-' + metric), 10);
+            }
+        })(metrics[i]);
+    }
+    return functions;
+})();
+
+
 $(function(){
     $grid = $('.list');
     $streamHead = $('#streamHead');
-    $total = $('#total');
+    for(var i = 0, l = metrics.length; i < l; i++){
+        $totals[metrics[i]] = $('#total-' + metrics[i]);
+        totals[metrics[i]] = parseInt($totals[metrics[i]].text(), 10);
+    }
+
+    $select = $('#metric-select').on('change', function(){
+        displayMode = $select.val();
+        var $lis = $grid.find('li');
+        $lis.each(function(){
+            var $this = $(this);
+            $this.find('.number').text($this.attr('data-' + displayMode));
+        });
+        $grid.isotope('updateSortData', $lis)
+            .isotope({ sortBy : displayMode });
+    });
 
     $grid.isotope({
-        sortBy: 'wings',
+        sortBy: 'all',
         itemSelector: 'li',
         sortAscending : false,
         animationEngine : 'best-available',
@@ -38,15 +85,7 @@ $(function(){
         masonry: {
             columnWidth: 80
         },
-        getSortData: {
-            wings: function($el){
-                return parseInt($el.find('.number').text(), 10);
-            }
-        }
+        getSortData: sortFunctions
     });
-
-    setTimeout(function(){
-       //$grid.isotope({ sortBy : 'wings' });
-    }, 2000);
 
 });
